2859c573f5d387d4af6b4dafe3e4a0a5d7254f97
git <coder@isbut.cn>
2021-02-09 21:02:38 +0800

    2月9号打卡

---
+++ leetcode70.java
@@ -0,0 +1,16 @@
+public class leetcode70 {
+    public static int climbStairs(int n) {
+        if (n <= 2) return n;
+        int j = 1;
+        int k = 2;
+        int temp = 0;
+        for (int i = 3; i <= n; i++) {
+            temp = j + k;
+            j = k;
+            k = temp;
+        }
+        return temp;
+    }
+
+
+}
