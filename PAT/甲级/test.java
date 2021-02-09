89bf803c633c4717f5108b08be9f684652b5c77b
git <coder@isbut.cn>
2021-02-09 16:40:11 +0800

    上传其他文件

---
+++ PAT/甲级/test.java
@@ -0,0 +1,19 @@
+package PAT.甲级;
+
+import java.util.*;
+
+
+public class test {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int N=scanner.nextInt();
+        int sum=0,day=0;
+        for (int i=1;day<N;i++){
+             for (int j=1;j<=i&&day<N;j++){
+                 sum+=i;
+                 day++;
+             }
+        }
+        System.out.println(sum);
+    }
+}
