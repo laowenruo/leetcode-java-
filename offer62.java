fec85253bd041024ac2d4d721e37eb33d1a8365a
git <coder@isbut.cn>
2021-02-13 17:57:51 +0800

    初一打卡

---
+++ offer62.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class offer62 {
+    public int lastRemaining(int n, int m) {
+        ArrayList<Integer> list=new ArrayList<>();
+        for (int i=0;i<n;i++) list.add(i);
+        int p=0;
+        while (list.size()!=1){
+            p=((p+2)%list.size());
+            list.remove(p);
+        }
+        return list.get(0);
+    }
+}
