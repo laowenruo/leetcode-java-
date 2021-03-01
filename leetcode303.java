a411f28fa959fe8caf08ca85e579ae423148bbb0
coder_ryan <1173779946@qq.com>
2021-03-01 22:49:38 +0800

    3.01打卡

---
+++ leetcode303.java
@@ -0,0 +1,12 @@
+public class leetcode303 {
+    private int[] nums;
+    public leetcode303(int[] nums) {
+        this.nums=nums;
+    }
+
+    public int sumRange(int i, int j) {
+        int sum=0;
+        for (;i<=j;i++) sum+=nums[i];
+        return sum;
+    }
+}
