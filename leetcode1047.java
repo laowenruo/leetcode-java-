c8d376ba028dc7bb47f7256428a40bdeb2e26171
coder_ryan <1173779946@qq.com>
2021-03-09 23:01:15 +0800

    3.09打卡

---
+++ leetcode1047.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+public class leetcode1047 {
+    public String removeDuplicates(String S) {
+        if (S.length()==1) return S;
+        Stack<Character> stack=new Stack<>();
+        char[] chars=S.toCharArray();
+        StringBuilder str=new StringBuilder();
+        for (int i=0;i<chars.length;i++){
+            if (stack.size()!=0&&stack.peek()==chars[i]) stack.pop();
+            else stack.push(chars[i]);
+        }
+
+        while (stack.size()!=0){
+            str.insert(0,stack.pop());
+        }
+        return str.reverse().toString();
+    }
+}
