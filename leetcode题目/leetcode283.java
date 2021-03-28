d50729bc4a19520216aa01692d6a15b2f957feae
coder_ryan <1173779946@qq.com>
2021-03-28 09:36:41 +0800

    练手熟悉度

--- leetcode题目/leetcode283.java
+++ leetcode题目/leetcode283.java
@@ -1,22 +1,15 @@
 package leetcode题目;
 
-import java.io.*;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.UnknownHostException;
-
 public class leetcode283 {
-    class Solution {
-        public void moveZeroes(int[] nums) {
-          if (nums==null||nums.length<=1) return;
-          int index=0;
-          for (int i=0;i< nums.length;i++){
-              if (nums[i]!=0) {
-                  nums[index++]=nums[i];
-              }
-          }
-          for (int i=index;i<nums.length;i++) nums[i]=0;
-        }
+    public void moveZeroes(int[] nums) {
+        int temp=0;
+        if(nums.length<=1||nums==null) return;
+        for (int i=0;i< nums.length;i++){
+                    if (nums[i]!=0) nums[temp++]=nums[i];
+                    else {
+                        continue;
+                    }
+    }
+        for (int i=temp;i<nums.length;i++) nums[i]=0;
     }
 }
